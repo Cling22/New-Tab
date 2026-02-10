5eb6d3bde1be54b24dc67484993ec233cb71c5b4
Cling.Py <154641389+cling26@users.noreply.github.com>
2026-02-10 18:40:38 -0500

    Add game links to games.js

---
+++ games.js
@@ -0,0 +1,26 @@
+window.PAGES = {
+
+  "Polytrack": "https://t3m1n4l.github.io/polytrack/",
+  
+  "Drive Mad": "https://cling26.github.io/drive-mad/drive-mad-new/",
+  
+  "Snow Rider": "https://jigar1234nob.github.io/snowrider3d/",
+
+  "Smash Karts": "https://educational-moyv.github.io/smash-karts/",
+  
+  "Basket Bros": "https://hacker-114.github.io/basket-bros/",
+  
+  "Rocket League 3D": "https://cling26.github.io/SmashKarts/",
+
+  "Geometry Dash": "https://gamehippy.github.io/geometry-dash-unblocked/",
+  
+  "Basket Random": "https://unblocked-games-786.github.io/Basket-Random/",
+  
+  "Eggy Car": "https://cling26.github.io/EggyCar/",
+  
+  "Color Tunnel 2": "https://cling26.github.io/ColorTunnel2/",
+
+  "Retro Bowl": "https://cling26.github.io/rb/",
+  
+  "Slots": "https://cling26.github.io/Slot-Machine/" // Last
+};
